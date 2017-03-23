153d88bcc35d541ac5bab061aa9312113daf4480
rfrieds <rfriedman@Rachels-MacBook-Air.local>
2017-03-22 21:30:04 -0400

    Melissa Alt Cakes Website

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+
+	$(".variable-width").slick({
+		autoplay: true,
+		autoplaySpeed: 3000,
+		infinite: true,
+		slidesToShow: 1,
+		centerMode: true,
+		variableWidth: true
+	});
+
+});
